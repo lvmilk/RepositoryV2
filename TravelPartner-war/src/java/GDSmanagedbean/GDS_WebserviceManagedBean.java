e5bd59bc7b7de9cd34b1d445bf66226cc906f379
LI HAO <LI HAO@LIHAO-PC>
2015-11-17 23:37:10 +0800

    add new pages

--- TravelPartner-war/src/java/GDSmanagedbean/GDS_WebserviceManagedBean.java
+++ TravelPartner-war/src/java/GDSmanagedbean/GDS_WebserviceManagedBean.java
@@ -8,6 +8,8 @@ package GDSmanagedbean;
 import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.xml.ws.WebServiceRef;
+import sessionbean.gds.GDSLoginBean_Service;
 
 /**
  *
@@ -17,5 +19,14 @@ import javax.inject.Named;
 @Named(value = "gdsWs")
 @ViewScoped
 public class GDS_WebserviceManagedBean implements Serializable{
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/GDSLoginBean/GDSLoginBean.wsdl")
+    private GDSLoginBean_Service service;
+
+    private boolean publishFlight(java.lang.String flightNo, javax.xml.datatype.XMLGregorianCalendar flightDate, javax.xml.datatype.XMLGregorianCalendar depTime, javax.xml.datatype.XMLGregorianCalendar arrTime, java.lang.String depAirport, java.lang.String arrAirport, java.lang.String depIATA, java.lang.String arrIATA, java.lang.Integer seatQuota) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        sessionbean.gds.GDSLoginBean port = service.getGDSLoginBeanPort();
+        return port.publishFlight(flightNo, flightDate, depTime, arrTime, depAirport, arrAirport, depIATA, arrIATA, seatQuota);
+    }
     
 }
