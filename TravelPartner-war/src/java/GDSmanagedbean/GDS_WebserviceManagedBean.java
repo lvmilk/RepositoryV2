26729a3f48e62792d4248564f5d60bd7d6818c38
LI HAO <LI HAO@172.25.97.112>
2015-11-18 16:11:39 +0800

    setup airline and gds flight entity

--- TravelPartner-war/src/java/GDSmanagedbean/GDS_WebserviceManagedBean.java
+++ TravelPartner-war/src/java/GDSmanagedbean/GDS_WebserviceManagedBean.java
@@ -15,19 +15,22 @@ import sessionbean.gds.GDSLoginBean_Service;
  *
  * @author LI HAO
  */
-
 @Named(value = "gdsWs")
 @ViewScoped
-public class GDS_WebserviceManagedBean implements Serializable{
+public class GDS_WebserviceManagedBean implements Serializable {
+
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/GDSLoginBean/GDSLoginBean.wsdl")
     private GDSLoginBean_Service service;
+    
+    public GDS_WebserviceManagedBean(){
+    }
+    
 
-
-    private boolean publishFlight(java.lang.String flightNo, javax.xml.datatype.XMLGregorianCalendar flightDate, javax.xml.datatype.XMLGregorianCalendar depTime, javax.xml.datatype.XMLGregorianCalendar arrTime, java.lang.String depAirport, java.lang.String arrAirport, java.lang.String depIATA, java.lang.String arrIATA, java.lang.Integer seatQuota, java.lang.String companyIATA) {
+    public boolean publishFlight(java.lang.String flightNo, javax.xml.datatype.XMLGregorianCalendar flightDate, javax.xml.datatype.XMLGregorianCalendar depTime, javax.xml.datatype.XMLGregorianCalendar arrTime, java.lang.String depAirport, java.lang.String arrAirport, java.lang.String depIATA, java.lang.String arrIATA, java.lang.Integer seatQuota, java.lang.String companyIATA) {
         // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
         // If the calling of port operations may lead to race condition some synchronization is required.
         sessionbean.gds.GDSLoginBean port = service.getGDSLoginBeanPort();
         return port.publishFlight(flightNo, flightDate, depTime, arrTime, depAirport, arrAirport, depIATA, arrIATA, seatQuota, companyIATA);
     }
-    
+
 }
